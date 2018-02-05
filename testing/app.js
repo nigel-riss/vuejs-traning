dc208fde66592a763623568611073be3f10f2d4a
Yuriy Kurenkov <mr.kurenkov@gmail.com>
2018-02-05 12:25:03 +0200

    Setup Vue workflow

--- testing/app.js
+++ testing/app.js
@@ -1,56 +1,35 @@
-
-Vue.component('hello', {
-  template: '<h1>Hello!</h1>'
-});
-
-var vm1 = new Vue({
-  // el: '#app1',
+new Vue({
+  el: "#app",
   data: {
-    title: 'The VueJS Instance',
-    showParagraph: false
+    title: 'The VueJS Instance'
+  },
+  beforeCreate: function() {
+    console.log('beforeCreate()');
+  },
+  created: function() {
+    console.log('created()');
+  },
+  beforeMount: function() {
+    console.log('beforeMount()');
+  },
+  mounted: function() {
+    console.log('mounted()');
+  },
+  beforeUpdate: function() {
+    console.log('beforeUpdate');
+  },
+  updated: function() {
+    console.log('update()');
+  },
+  beforeDestroy: function() {
+    console.log('beforeDestroy()');
+  },
+  destroyed: function() {
+    console.log('destroyed()');
   },
   methods: {
-    show: function() {
-      this.showParagraph = true;
-      this.updateTitle('The VueJS Instance (Updated)');
-    },
-    updateTitle: function(title) {
-      this.title = title;
-    }
-  },
-  computed: {
-    lowercaseTitle: function() {
-      return this.title.toLowerCase();
-    }
-  },
-  watch: {
-    title: function(value) {
-      alert('Title changed, new value: ' + value);
+    destroy: function() {
+      this.$destroy();
     }
   }
-});
-
-// vm1.$refs.heading.innerText = "Something Else";
-// console.log(vm1);
-vm1.$mount("#app1");
-
-var vm2 = new Vue({
-  el: "#app2",
-  data: {
-    title: "The second Instance"
-  },
-  methods: {
-    onChange: function() {
-      vm1.title = "Changed!";
-    }
-  }
-});
-
-// var vm3 = new Vue({
-//   el: '.hello',
-//   template: '<h1>Hello!</h1>'
-// });
-
-// vm3.$mount("#app3");
-// vm3.$mount();
-// document.getElementById("app3").appendChild(vm3.$el);
+})
