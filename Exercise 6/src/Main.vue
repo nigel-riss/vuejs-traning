b13667dc6747df5920fec460ee9eb984fef6a8e1
nigel-riss <mr.kurenkov@gmail.com>
2018-02-16 18:23:12 +0200

    Finish Exercise 6

---
+++ Exercise 6/src/Main.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="row">
+        <app-server-status></app-server-status>
+        <app-server-details></app-server-details>
+    </div>
+</template>
+
+<script>
+import ServerStatus from './ServerStatus.vue';
+import ServerDetails from './ServerDetails.vue';
+export default {
+    components: {
+        'app-server-status': ServerStatus,
+        'app-server-details': ServerDetails
+    }
+}
+</script>
+
