2d8b6484313277eba0b5255bc3fe308e91d68aab
nigel-riss <mr.kurenkov@gmail.com>
2018-02-22 20:14:20 +0200

    Sync mid section

--- communication/src/components/UserDetail.vue
+++ communication/src/components/UserDetail.vue
@@ -2,10 +2,25 @@
     <div class="component">
         <h3>You may view the User Details here</h3>
         <p>Many Details</p>
+        <p>User Name: {{ switchName() }}</p>
     </div>
 </template>
 
 <script>
+    export default {
+        props: {
+            name: {
+                type: String,
+                // required: true,
+                default: 'Max'
+            }
+        },
+        methods: {
+            switchName() {
+                return this.name.split('').reverse().join('');
+            }
+        }
+    }
 </script>
 
 <style scoped>
